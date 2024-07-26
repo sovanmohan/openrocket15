a4d73ee04452a98a8cb33655d228129db61b1244
sovanmohan <msovan928@gmail.com>
2024-07-26 13:08:30 +0530

    Commit message goes here

---
+++ src/test/java/com/github/sovanmohan/openrocket15/Openrocket15Test.java
@@ -0,0 +1,38 @@
+package com.github.sovanmohan.openrocket15;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Openrocket15Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Openrocket15Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Openrocket15Test.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
